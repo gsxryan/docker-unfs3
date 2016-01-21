a4949eb8ec7c803b71938ef0e20c94af4bc3f2a2
voobscout <voobscout@archlinux.info>
2016-01-21 15:00:21 +0100

    script tweak

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+arr_join() {
+    # $1 is return variable name
+    # $2 is sep
+    # $3... are the elements to join
+    local retname=$1 sep=$2 ret=$3
+    shift 3 || shift $(($#))
+    printf -v "$retname" "%s" "$ret${@/#/$sep}"
+}
 
 # service rpcbind start
 rpcbind
@@ -11,4 +19,6 @@ do
     mounts+=${name%%" "*}" "
 done < /etc/exports
 
-eval "inotifywait -rm ${mounts}"
+arr_join watchable " " "${mounts[@]}"
+
+inotifywait -rm ${watchable}
